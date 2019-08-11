bb338e79405cd08d4360f038338e754ed0e026cc
Erica Palosaari <palosaari.erica@gmail.com>
2019-08-11 13:13:10 +0200

    Added Media folder and items

---
+++ Media/Media.lua
@@ -0,0 +1,19 @@
+local A, C = unpack(select(2, ...))
+
+C["Media"] = {
+    -- Fonts
+    ["SEMPRG__"] = [[Interface\AddOns\AmexiUI\Media\Fonts\SEMPRG__.ttf]],
+    ["Standard"] = [[Interface\AddOns\AmexiUI\Media\Fonts\standard.ttf]],
+    ["BebasNeue"] = [[Interface\AddOns\AmexiUI\Media\Fonts\BebasNeue.ttf]],
+    ["Ubuntu_Light"] = [[Interface\AddOns\AmexiUI\Media\Fonts\Ubuntu-L.ttf]],
+
+    -- Textures
+    ["fer28"] = [[Interface\AddOns\AmexiUI\Media\Textures\fer28]],
+    ["AUIIconBorder"] = [[Interface\AddOns\AmexiUI\Media\Textures\iconborder]],
+    ["AUIBackdrop"] = [[Interface\AddOns\AmexiUI\Media\Textures\backdrop]],
+    ["AUIPowerBar"] = [[Interface\AddOns\AmexiUI\Media\Textures\Minimalist]],
+    ["AUIHighlight"] = [[Interface\AddOns\AmexiUI\Media\Textures\raidbg]],
+
+    -- Colors
+    
+}
